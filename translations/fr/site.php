1b40c29ad5116041caf5d20982af2b3d24c1aec5
Julien Lambert-Charette <julien.lambert-charette@canada.ca>
2020-01-12 22:15:09 -0500

    Add Pages, Contact, Search Form, Dashboard Widgets, Articles

--- translations/fr/site.php
+++ translations/fr/site.php
@@ -1,5 +1,20 @@
 <?php
 
 return [
-    "Home" => "Accueil"
+    "Home" => "Accueil",
+    "Undefined content" => "Contenu introuvable",
+    "Journalism Login" => "Connexion Journaliste",
+    "Members Area" => "Espace Membre",
+    "News" => "Nouvelles",
+    "Learn More" => "En savoir plus",
+    "Our Services" => "Nos services",
+    "Our Newspapers" => "Nos journaux",
+    "Recent News" => "Nouvelles récentes",
+    "My Dashboard" => "Tableau de bord",
+
+    // Search
+    "Search" => "Recherche",
+    "result(s) found:" => "résultat(s) trouvé(s) :",
+    "You are searching for:" => "Vous recherchez :",
+    "Search this website" => "Rechercher ce site web",
 ];
