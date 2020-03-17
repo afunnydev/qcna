34819a0371d7b0a6e2717fb72dff27857d0b198a
Julien Lambert-Charette <julien.lambert-charette@canada.ca>
2020-03-17 15:55:01 -0400

    Canada Local news fine tuning.

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,16 @@ import Icons from 'uikit/dist/js/uikit-icons';
 UIkit.use(Icons);
 
 $(document).ready(function() {
-    // Load news on the homepage
-    if ($('#homepage-news-container').length) {
-        let output = "";
-        $.get('https://qcna.omerlocdn.com/production/qcna/site/recent-articles-list.json ', function(data) {
-            $.each(data['contents'], function(i, item) {
+    // Load news
+    if ($('.ajax-news-container').length) {
+        let newsOutput = "";
+        let payload = "https://qcna.omerlocdn.com/production/qcna/site/recent-articles-list.json";
+
+        $.get(payload, function(data) {
+            $.each(data['contents'], function(index, item) {
+                let title = item.title.en;
+                let lead = $.trim(item.lead.en).substring(0, 120).split(" ").slice(0, -1).join(" ") + "...";
+
                 let article =
                 `<div>
                     <article class="uk-card uk-card-default uk-card-hover uk-card-small uk-background-muted">
@@ -24,21 +29,24 @@ $(document).ready(function() {
                                 <span uk-icon="icon: calendar"></span> <time datetime="${item.publicationDate}">${item.publicationDate.split('T')[0]}</time>
                             </p>
                             <h5 class="uk-card-title">
-                                <a class="uk-link-heading" href="${item.canonicalUrlOverride}" target="_blank">${item.title.en}</a>
+                                <a class="uk-link-heading" href="${item.canonicalUrlOverride}" target="_blank">${title}</a>
                             </h5>
-                            <p>${item.lead.en}</p>
+                            <p>${lead}</p>
                         </div>
                         <div class="uk-card-footer">
                             <a href="${item.canonicalUrlOverride}" class="uk-button uk-button-text" target="_blank">Read more</a>
                         </div>
                     </article>
-                </div>`
-                output += article;
-                return i < 3;
+                </div>`;
+                newsOutput += article;
+                return index < 3;
             });
-            $('#homepage-news-container').append(output);
-        });
+
+            $('.ajax-news-container').append(newsOutput);
+        })
+
     }
+
     // Load news in the footer
     if ($('#footer-news-container').length) {
         let output = "";
