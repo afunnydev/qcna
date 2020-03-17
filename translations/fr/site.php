b6bbbfc32a6300f34fd150e5c2314f7224ddc0e7
Julien Lambert-Charette <julien.lambert-charette@canada.ca>
2020-03-16 23:06:11 -0400

    Add fine tuning.

--- translations/fr/site.php
+++ translations/fr/site.php
@@ -11,10 +11,38 @@ return [
     "Our Newspapers" => "Nos journaux",
     "Recent News" => "Nouvelles récentes",
     "My Dashboard" => "Tableau de bord",
+    "Learn more" => "En savoir plus",
+    "Keep in touch with us" => "Rester en contact avec nous",
+    "Form has been submitted successfully!" => "Le formulaire a été envoyé avec succès!",
+    "Error! Please review the form and try submitting again." => "Erreur! Veuillez vérifier le formulaire avant de l'envoyer à nouveau.",
+    "Please confirm you're not a robot..." => "Confirmez que nous n'êtes pas un robot...",
+    "Submit" => "Envoyer",
+    "Contact Details" => "Information de contact",
+    "Email Us" => "Courriel",
+    "Visit Website" => "Visiter le site web",
+    "Subscribers" => "Abonnés",
 
     // Search
     "Search" => "Recherche",
     "result(s) found:" => "résultat(s) trouvé(s) :",
     "You are searching for:" => "Vous recherchez :",
     "Search this website" => "Rechercher ce site web",
+
+    // News
+    "Previous" => "Précédent",
+    "Next" => "Suivant",
+    "More news" => "Plus de nouvelles",
+    "Share this news" => "Partagez cette nouvelle",
+    "Canada Local News" => "Nouvelles de Canada Local",
+    "QCNA News" => "Nouvelles de QCNA",
+    "LJI News" => "Nouvelles du LJI",
+
+    // Error pages
+    "Back to homepage" => "Retour à l'accueil",
+    "Access Denied" => "Accès refusé",
+    "Sorry, you cannot access the content of the requested page." => "Désolé, vous ne pouvez accéder à la page demandée.",
+    "Sorry, the page you are looking for does not exist." => "Désolé, la page que vous recherchez n'existe pas.",
+    "Page Not Found" => "Page introuvable",
+    "Sorry, something wrong happened. Please contact the webmaster." => "Désolé, il y a un problème avec le serveur web.",
+    "Internal Server Error" => "Erreur Serveur Web",
 ];
